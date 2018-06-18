07d55cde102153eb43758b307a47cc6005c68eff
eduobusunum <40331702+eduobusunum@users.noreply.github.com>
2018-06-17 22:10:37 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 var codes = ["1515175", "74656", "123213"];
 var codesIndicies = [1,2,3];
-var names = ["Secret Tunnel", "SecretTunnel", "secrettunnel", "secrettunnel",
+
+var names = ["Secret Tunnel", "SecretTunnel", "secrettunnel", "secret tunnel",
 "Ihaveabadfeelingaboutthis", "ihaveabadfeelingaboutthis", "I have a bad feeling about this", "i have a bad feeling about this",
 "So Good", "so good", "sogood", "SoGood"];
 var namesIndicies = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3];
@@ -14,24 +15,23 @@ function checkName() {
   var currentName = document.getElementById("name").value;
   id = "name";
   validateValue(id,currentName, names);
-//  if ( currentName == "two"){
-//   document.getElementById("name").style.color = "green";}
-//  else {
-//  document.getElementById("name").style.color = "black";
-//  }
 }
 
 
 function checkCode() {
   var currentCode = document.getElementById("code").value;
   id = "code";
-  validateValue(id, currentCode, codes);
-//  if ( currentCode == "three"){
-//   document.getElementById("code").style.color = "green";}
-//  else {
-//  document.getElementById("code").style.color = "black";
-// }
-}
+  console.log(codes);
+  var a = currentCode.indexOf(codes);
+  console.log(currentCode);
+  console.log(a);
+  console.log(id);
+  if (a == -1){
+    document.getElementById(id).style.color = "black";}
+  else{
+    document.getElementById(id).style.color = "green";}
+ }
+
 
 function validateValue (id, input, storedArray){
   console.log(storedArray);
